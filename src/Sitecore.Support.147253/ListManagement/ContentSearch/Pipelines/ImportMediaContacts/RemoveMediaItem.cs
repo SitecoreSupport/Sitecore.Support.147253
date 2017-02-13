ba9335b55f72ada35b94c73b1e105454b872ed5d
sc-spl <spl@sitecore.net>
2017-02-13 11:03:12 +0200

    Initial commit

---
+++ src/Sitecore.Support.147253/ListManagement/ContentSearch/Pipelines/ImportMediaContacts/RemoveMediaItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SqlClient;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.ListManagement.ContentSearch.Pipelines;
+
+namespace Sitecore.Support.ListManagement.ContentSearch.Pipelines.ImportMediaContacts
+{
+    public class RemoveMediaItem : ListProcessor
+    {
+        public virtual void Process(ImportContactsBaseArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            Assert.IsNotNullOrEmpty(args.ImportFileId, "args.ImportFileId");
+            Item item = base.Database.GetItem(args.ImportFileId);
+            if (item != null)
+            {
+                try
+                {
+                    item.Delete();
+                }
+                catch
+                {
+                    Log.Warn("Sitecore.Support.147253 The Sitecore.Support.ListManagement.ContentSearch.Pipelines.ImportMediaContacts processor was interrupted ", this);
+                }
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.147253/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.147253")]
+[assembly: AssemblyProduct("Sitecore.Support.147253")]
+[assembly: ComVisible(false)]
